ada23411b1ea5f7ea6195a51100ea2e143d9cfc3
Chase Burnham <cburnhamiv@gmail.com>
2018-11-25 09:04:32 -0700

    Initial Commit

---
+++ lh-helpers/lh_helpers.h
@@ -0,0 +1,19 @@
+//
+//  lh_helpers.h
+//  lh-helpers
+//
+//  Created by Carl Burnham on 11/25/18.
+//  Copyright © 2018 Carl Burnham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for lh_helpers.
+FOUNDATION_EXPORT double lh_helpersVersionNumber;
+
+//! Project version string for lh_helpers.
+FOUNDATION_EXPORT const unsigned char lh_helpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lh_helpers/PublicHeader.h>
+
+
---
+++ lh-helpersTests/lh_helpersTests.swift
@@ -0,0 +1,34 @@
+//
+//  lh_helpersTests.swift
+//  lh-helpersTests
+//
+//  Created by Carl Burnham on 11/25/18.
+//  Copyright © 2018 Carl Burnham. All rights reserved.
+//
+
+import XCTest
+@testable import lh_helpers
+
+class lh_helpersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
