87a432e5b59be6f988a1387d32d6178458d1aefe
Chase Burnham <cburnhamiv@gmail.com>
2018-12-04 17:52:28 -0500

    made items public

--- lh-helpers/Observers/Observerables.swift
+++ lh-helpers/Observers/Observerables.swift
@@ -9,36 +9,38 @@
 // Adapted from: https://five.agency/solving-the-binding-problem-with-swift/
 import Foundation
 
-class Observable<T> {
-    var value: T {
+public class Observable<T> {
+    public var value: T {
         didSet {
             for bondBox in bonds {
-                bondBox.bond?.valueChanged(value)
+                DispatchQueue.main.async {
+                    bondBox.bond?.valueChanged(self.value)
+                }
             }
         }
     }
     
     var bonds: [BondBox<T>] = []
     
-    init(_ v: T) {
+    public init(_ v: T) {
         value = v
     }
 }
 
 // Put the observation callback in a Bond so it is not part of the model and does no exist when the view does not exist
-class Bond<T> {
-    var valueChanged: (T) -> ()
+public class Bond<T> {
+    public var valueChanged: (T) -> ()
     
-    init(_ valueChanged: @escaping (T) -> ()) {
+    public init(valueChanged: @escaping (T) -> ()) {
         self.valueChanged = valueChanged
     }
     
-    func bind(observable: Observable<T>) {
+    public func bind(observable: Observable<T>) {
         observable.bonds.append(BondBox(self))
     }
 }
 
-class BondBox<T> {
+public class BondBox<T> {
     weak var bond: Bond<T>?
     init(_ b: Bond<T>) { bond = b }
 }
