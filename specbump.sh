91cf40404947f7adc7b2a525fb5421f34188c13f
Chase Burnham <cburnhamiv@gmail.com>
2019-07-12 16:20:17 -0400

    updated version to 1.4.2

--- specbump.sh
+++ specbump.sh
@@ -5,7 +5,7 @@ function specbump() {
     git add .
     git commit -m "updated version to $1"
     git push
-    git tag '$1'
+    git tag $1
     git push --tags
     pod trunk push LhHelpers.podspec --allow-warnings
 }
